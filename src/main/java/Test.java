27c08f695adf64afbadb6389047bcbe51696fc90
shu <shu@qq.comm>
2018-03-29 22:40:07 +0800

    2 change

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,5 +7,6 @@ public class Test {
         System.out.println("2 change");
         System.out.println("1 final");
         System.out.println("final");
+        System.out.println("branch 1");
     }
 }
